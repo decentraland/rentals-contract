9df5a263bbe5ebaf611f8ad2fb3f5f6b5410eedb
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-02-09 19:25:59 -0300

    feat: Add reject signatures function

--- contracts/Rentals.sol
+++ contracts/Rentals.sol
@@ -5,7 +5,47 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract Rentals is OwnableUpgradeable {
-  function initialize(address _owner) external initializer {
-    _transferOwnership(_owner);
-  }
+    mapping(bytes => bool) public isSignatureRejected;
+
+    struct RenterParams {
+        bytes sig;
+    }
+
+    struct TenantParams {
+        bytes sig;
+    }
+
+    function initialize(address _owner) external initializer {
+        _transferOwnership(_owner);
+    }
+
+    function rent(
+        RenterParams calldata _renterParams,
+        TenantParams calldata _tenantParams
+    ) external {
+        bytes[] memory sigs;
+
+        sigs[0] = _renterParams.sig;
+        sigs[1] = _tenantParams.sig;
+
+        rejectSignatures(sigs);
+    }
+
+    function rejectSignatures(bytes[] memory _sigs) public {
+        require(
+            _sigs.length > 0,
+            "Rentals#rejectSignatures: EMPTY_SIGNATURE_ARRAY"
+        );
+
+        for (uint256 i = 0; i < _sigs.length; i++) {
+            bytes memory _sig = _sigs[i];
+
+            require(
+                !isSignatureRejected[_sig],
+                "Rentals#rejectSignature: ALREADY_REJECTED"
+            );
+
+            isSignatureRejected[_sig] = true;
+        }
+    }
 }
--- test/Rentals.spec.ts
+++ test/Rentals.spec.ts
@@ -31,4 +31,45 @@ describe("Rentals", () => {
       ).to.be.revertedWith("Initializable: contract is already initialized");
     });
   });
+
+  describe("rejectSignatures", () => {
+    let sig: string;
+
+    beforeEach(async () => {
+      sig = "0x7369676e6174757265"; // Hex for "signature"
+    });
+
+    it("should set isSignatureRejected mapping value for the provided signature to true", async () => {
+      await rentals.connect(deployer).initialize(owner.address);
+      await rentals.rejectSignatures([sig]);
+      const isSignatureRejected = await rentals.isSignatureRejected(sig);
+      expect(isSignatureRejected).to.be.true;
+    });
+
+    it("should set isSignatureRejected mapping value for all the provided signatures to true", async () => {
+      await rentals.connect(deployer).initialize(owner.address);
+      const anotherSig = "0x616e6f746865722d7369676e6174757265";
+      await rentals.rejectSignatures([sig, anotherSig]);
+      const res = await Promise.all([
+        rentals.isSignatureRejected(sig),
+        rentals.isSignatureRejected(anotherSig),
+      ]);
+      expect(res.every((isRejected) => isRejected)).to.be.true;
+    });
+
+    it("should revert when no signatures are provided", async () => {
+      await rentals.connect(deployer).initialize(owner.address);
+      await expect(rentals.rejectSignatures([])).to.be.revertedWith(
+        "Rentals#rejectSignatures: EMPTY_SIGNATURE_ARRAY"
+      );
+    });
+
+    it("should revert when the signature was already rejected", async () => {
+      await rentals.connect(deployer).initialize(owner.address);
+      await rentals.rejectSignatures([sig]);
+      await expect(rentals.rejectSignatures([sig])).to.be.revertedWith(
+        "Rentals#rejectSignature: ALREADY_REJECTED"
+      );
+    });
+  });
 });
