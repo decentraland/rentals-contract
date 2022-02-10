88dc6fee0bf20890923882f11b19abd3a0c02a08
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-02-10 16:50:47 -0300

    feat: Validate signatures in rent

--- contracts/Rentals.sol
+++ contracts/Rentals.sol
@@ -3,26 +3,97 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
 
-contract Rentals is OwnableUpgradeable {
+import "hardhat/console.sol";
+
+contract Rentals is OwnableUpgradeable, EIP712Upgradeable {
+    // Constants
+    bytes32 private constant RENTER_SIGN_DATA_TYPEHASH =
+        keccak256(
+            bytes(
+                "RenterSignData(address renter,uint256 maxDays,uint256 price,uint256 expiration,address _contract,uint256 tokenId,bytes32 salt)"
+            )
+        );
+    bytes32 private constant TENANT_SIGN_DATA_TYPEHASH =
+        keccak256(
+            bytes(
+                "TenantSignData(address tenant,uint256 _days,uint256 expiration,address _contract,uint256 tokenId,bytes32 salt)"
+            )
+        );
+
+    // State variables
     mapping(bytes => bool) public isSignatureRejected;
 
+    // Structs
     struct RenterParams {
+        address renter;
+        uint256 maxDays;
+        uint256 price;
+        uint256 expiration;
+        address _contract;
+        uint256 tokenId;
+        bytes32 salt;
         bytes sig;
     }
 
     struct TenantParams {
+        address tenant;
+        uint256 _days;
+        uint256 expiration;
+        address _contract;
+        uint256 tokenId;
+        bytes32 salt;
         bytes sig;
     }
 
+    // Public functions
     function initialize(address _owner) external initializer {
         _transferOwnership(_owner);
+        __EIP712_init("Rentals", "1");
     }
 
-    function rent(
-        RenterParams calldata _renterParams,
-        TenantParams calldata _tenantParams
-    ) external {
+    function rent(RenterParams calldata _renterParams, TenantParams calldata _tenantParams) external {
+        // Validate renter signature
+        bytes32 renterMessageHash = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    RENTER_SIGN_DATA_TYPEHASH,
+                    _renterParams.renter,
+                    _renterParams.maxDays,
+                    _renterParams.price,
+                    _renterParams.expiration,
+                    _renterParams._contract,
+                    _renterParams.tokenId,
+                    _renterParams.salt
+                )
+            )
+        );
+
+        address renter = ECDSAUpgradeable.recover(renterMessageHash, _renterParams.sig);
+
+        require(renter == _renterParams.renter, "Rentals#rent: SIGNER_NOT_RENTER");
+
+        // Validate tenant signature
+        bytes32 tenantMessageHash = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    TENANT_SIGN_DATA_TYPEHASH,
+                    _tenantParams.tenant,
+                    _tenantParams._days,
+                    _tenantParams.expiration,
+                    _tenantParams._contract,
+                    _tenantParams.tokenId,
+                    _tenantParams.salt
+                )
+            )
+        );
+
+        address tenant = ECDSAUpgradeable.recover(tenantMessageHash, _tenantParams.sig);
+
+        require(tenant == _tenantParams.tenant, "Rentals#rent: SIGNER_NOT_TENANT");
+
+        // Reject signatures so they cannot be used again
         bytes[] memory sigs = new bytes[](2);
 
         sigs[0] = _renterParams.sig;
@@ -32,18 +103,12 @@ contract Rentals is OwnableUpgradeable {
     }
 
     function rejectSignatures(bytes[] memory _sigs) public {
-        require(
-            _sigs.length > 0,
-            "Rentals#rejectSignatures: EMPTY_SIGNATURE_ARRAY"
-        );
+        require(_sigs.length > 0, "Rentals#rejectSignatures: EMPTY_SIGNATURE_ARRAY");
 
         for (uint256 i = 0; i < _sigs.length; i++) {
             bytes memory _sig = _sigs[i];
 
-            require(
-                !isSignatureRejected[_sig],
-                "Rentals#rejectSignature: ALREADY_REJECTED"
-            );
+            require(!isSignatureRejected[_sig], "Rentals#rejectSignature: ALREADY_REJECTED");
 
             isSignatureRejected[_sig] = true;
         }
--- test/Rentals.spec.ts
+++ test/Rentals.spec.ts
@@ -1,89 +1,182 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { expect } from "chai";
-import { ethers } from "hardhat";
-import { Rentals } from "../typechain-types/Rentals";
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
+import { Rentals } from '../typechain-types/Rentals'
 
-describe("Rentals", () => {
-  let deployer: SignerWithAddress;
-  let owner: SignerWithAddress;
-  let rentals: Rentals;
+describe('Rentals', () => {
+  let deployer: SignerWithAddress
+  let owner: SignerWithAddress
+  let renter: SignerWithAddress
+  let tenant: SignerWithAddress
+  let rentals: Rentals
 
   beforeEach(async () => {
     // Store addresses
-    [deployer, owner] = await ethers.getSigners();
+    ;[deployer, owner, renter, tenant] = await ethers.getSigners()
 
     // Deploy Rentals contract
-    const RentalsFactory = await ethers.getContractFactory("Rentals");
-    rentals = await RentalsFactory.connect(deployer).deploy();
-  });
+    const RentalsFactory = await ethers.getContractFactory('Rentals')
+    rentals = await RentalsFactory.connect(deployer).deploy()
+  })
 
-  describe("initialize", () => {
-    it("should set the owner", async () => {
-      await rentals.connect(deployer).initialize(owner.address);
-      const rentalsOwner = await rentals.owner();
-      expect(rentalsOwner).to.be.equal(owner.address);
-    });
+  describe('initialize', () => {
+    it('should set the owner', async () => {
+      await rentals.connect(deployer).initialize(owner.address)
+      const rentalsOwner = await rentals.owner()
+      expect(rentalsOwner).to.be.equal(owner.address)
+    })
 
-    it("should revert when initialized more than once", async () => {
-      await rentals.connect(deployer).initialize(owner.address);
-      await expect(
-        rentals.connect(deployer).initialize(owner.address)
-      ).to.be.revertedWith("Initializable: contract is already initialized");
-    });
-  });
+    it('should revert when initialized more than once', async () => {
+      await rentals.connect(deployer).initialize(owner.address)
+      await expect(rentals.connect(deployer).initialize(owner.address)).to.be.revertedWith(
+        'Initializable: contract is already initialized'
+      )
+    })
+  })
 
-  describe("rent", () => {
-    it("should reject both renter and tenant signatures", async () => {
-      const sigA = "0x7369676e6174757265";
-      const sigB = "0x616e6f746865722d7369676e6174757265";
-      await rentals.connect(deployer).initialize(owner.address);
-      await rentals.rent({ sig: sigA }, { sig: sigB });
+  describe('rent', () => {
+    it('should reject both renter and tenant signatures', async () => {
+      const renterParams = {
+        renter: renter.address,
+        maxDays: '0',
+        price: '0',
+        expiration: '0',
+        _contract: rentals.address,
+        tokenId: '0',
+        salt: ethers.utils.randomBytes(32),
+      }
+
+      const renterSignature = await renter._signTypedData(
+        {
+          chainId: 31337,
+          name: 'Rentals',
+          verifyingContract: rentals.address,
+          version: '1',
+        },
+        {
+          RenterSignData: [
+            {
+              type: 'address',
+              name: 'renter',
+            },
+            {
+              type: 'uint256',
+              name: 'maxDays',
+            },
+            {
+              type: 'uint256',
+              name: 'price',
+            },
+            {
+              type: 'uint256',
+              name: 'expiration',
+            },
+            {
+              type: 'address',
+              name: '_contract',
+            },
+            {
+              type: 'uint256',
+              name: 'tokenId',
+            },
+            {
+              type: 'bytes32',
+              name: 'salt',
+            },
+          ],
+        },
+        renterParams
+      )
+
+      const tenantParams = {
+        tenant: tenant.address,
+        _days: '0',
+        expiration: '0',
+        _contract: rentals.address,
+        tokenId: '0',
+        salt: ethers.utils.randomBytes(32),
+      }
+
+      const tenantSignature = await tenant._signTypedData(
+        {
+          chainId: 31337,
+          name: 'Rentals',
+          verifyingContract: rentals.address,
+          version: '1',
+        },
+        {
+          TenantSignData: [
+            {
+              type: 'address',
+              name: 'tenant',
+            },
+            {
+              type: 'uint256',
+              name: '_days',
+            },
+            {
+              type: 'uint256',
+              name: 'expiration',
+            },
+            {
+              type: 'address',
+              name: '_contract',
+            },
+            {
+              type: 'uint256',
+              name: 'tokenId',
+            },
+            {
+              type: 'bytes32',
+              name: 'salt',
+            },
+          ],
+        },
+        tenantParams
+      )
+
+      await rentals.connect(deployer).initialize(owner.address)
+
+      await rentals.rent({ ...renterParams, sig: renterSignature }, { ...tenantParams, sig: tenantSignature })
       const res = await Promise.all([
-        rentals.isSignatureRejected(sigA),
-        rentals.isSignatureRejected(sigB),
-      ]);
-      expect(res.every((isRejected) => isRejected)).to.be.true;
-    });
-  });
+        rentals.isSignatureRejected(renterSignature),
+        rentals.isSignatureRejected(tenantSignature),
+      ])
+      expect(res.every((isRejected) => isRejected)).to.be.true
+    })
+  })
 
-  describe("rejectSignatures", () => {
-    let sig: string;
+  describe('rejectSignatures', () => {
+    let sig: string
 
     beforeEach(async () => {
-      sig = "0x7369676e6174757265"; // Hex for "signature"
-    });
+      sig = '0x7369676e6174757265' // Hex for "signature"
+    })
 
-    it("should set isSignatureRejected mapping value for the provided signature to true", async () => {
-      await rentals.connect(deployer).initialize(owner.address);
-      await rentals.rejectSignatures([sig]);
-      const isSignatureRejected = await rentals.isSignatureRejected(sig);
-      expect(isSignatureRejected).to.be.true;
-    });
+    it('should set isSignatureRejected mapping value for the provided signature to true', async () => {
+      await rentals.connect(deployer).initialize(owner.address)
+      await rentals.rejectSignatures([sig])
+      const isSignatureRejected = await rentals.isSignatureRejected(sig)
+      expect(isSignatureRejected).to.be.true
+    })
 
-    it("should set isSignatureRejected mapping value for all the provided signatures to true", async () => {
-      await rentals.connect(deployer).initialize(owner.address);
-      const anotherSig = "0x616e6f746865722d7369676e6174757265"; // Hex for another-signature
-      await rentals.rejectSignatures([sig, anotherSig]);
-      const res = await Promise.all([
-        rentals.isSignatureRejected(sig),
-        rentals.isSignatureRejected(anotherSig),
-      ]);
-      expect(res.every((isRejected) => isRejected)).to.be.true;
-    });
+    it('should set isSignatureRejected mapping value for all the provided signatures to true', async () => {
+      await rentals.connect(deployer).initialize(owner.address)
+      const anotherSig = '0x616e6f746865722d7369676e6174757265' // Hex for another-signature
+      await rentals.rejectSignatures([sig, anotherSig])
+      const res = await Promise.all([rentals.isSignatureRejected(sig), rentals.isSignatureRejected(anotherSig)])
+      expect(res.every((isRejected) => isRejected)).to.be.true
+    })
 
-    it("should revert when no signatures are provided", async () => {
-      await rentals.connect(deployer).initialize(owner.address);
-      await expect(rentals.rejectSignatures([])).to.be.revertedWith(
-        "Rentals#rejectSignatures: EMPTY_SIGNATURE_ARRAY"
-      );
-    });
+    it('should revert when no signatures are provided', async () => {
+      await rentals.connect(deployer).initialize(owner.address)
+      await expect(rentals.rejectSignatures([])).to.be.revertedWith('Rentals#rejectSignatures: EMPTY_SIGNATURE_ARRAY')
+    })
 
-    it("should revert when the signature was already rejected", async () => {
-      await rentals.connect(deployer).initialize(owner.address);
-      await rentals.rejectSignatures([sig]);
-      await expect(rentals.rejectSignatures([sig])).to.be.revertedWith(
-        "Rentals#rejectSignature: ALREADY_REJECTED"
-      );
-    });
-  });
-});
+    it('should revert when the signature was already rejected', async () => {
+      await rentals.connect(deployer).initialize(owner.address)
+      await rentals.rejectSignatures([sig])
+      await expect(rentals.rejectSignatures([sig])).to.be.revertedWith('Rentals#rejectSignature: ALREADY_REJECTED')
+    })
+  })
+})
