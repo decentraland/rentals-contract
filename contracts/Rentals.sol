b2789a5fd95fd1735150ca28d3d2ceb9bb60a23f
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-04-01 01:01:39 -0300

    chore: Remove import

--- contracts/Rentals.sol
+++ contracts/Rentals.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 
 import "./interfaces/IERC721Operable.sol";
 import "./interfaces/IERC721Verifiable.sol";
