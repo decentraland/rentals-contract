53f8463f36526882559f92695cfdab8e5d166565
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-04-01 13:40:16 -0300

    chore: Optimizer true

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import '@typechain/hardhat'
 import '@nomiclabs/hardhat-waffle'
 import 'solidity-coverage'
-import "hardhat-gas-reporter"
+import 'hardhat-gas-reporter'
 
 export default {
-  solidity: '0.8.7',
+  solidity: {
+    version: '0.8.7',
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
 }
