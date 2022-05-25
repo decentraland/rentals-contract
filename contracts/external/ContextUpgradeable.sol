0aaba923b4e610ae6fdd518b5202a93c963e7ba9
Nando <24811313+fzavalia@users.noreply.github.com>
2022-05-25 16:19:31 -0300

    feat: Get address from message data for meta trx

--- contracts/external/ContextUpgradeable.sol
+++ contracts/external/ContextUpgradeable.sol
@@ -20,8 +20,19 @@ abstract contract ContextUpgradeable is Initializable {
 
     function __Context_init_unchained() internal onlyInitializing {}
 
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
+    function _msgSender() internal view virtual returns (address sender) {
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(mload(add(array, index)), 0xffffffffffffffffffffffffffffffffffffffff)
+            }
+        } else {
+            sender = msg.sender;
+        }
+
+        return sender;
     }
 
     function _msgData() internal view virtual returns (bytes calldata) {
