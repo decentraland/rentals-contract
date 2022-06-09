767410c75c73c3e0aab8cb64e1292ac360235570
Nando <24811313+fzavalia@users.noreply.github.com>
2022-06-09 18:39:22 -0300

    chore: Update NV with more natspecs

--- contracts/commons/NonceVerifiable.sol
+++ contracts/commons/NonceVerifiable.sol
@@ -5,47 +5,42 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 abstract contract NonceVerifiable is OwnableUpgradeable {
-    // Nonces used to invalidate signatures.
+    /// @notice Current nonce at a contract level. Only updatable by the owner of the contract.
+    /// Updating it will invalidate all signatures created with the previous value on a contract level.
     uint256 public contractNonce;
-    // (signer address -> nonce)
+
+    /// @notice Current nonce per signer.
+    /// Updating it will invalidate all signatures created with the previous value on a signer level.
+    /// @custom:schema (signer address -> nonce)
     mapping(address => uint256) public signerNonce;
-    // (contract address -> token id -> signer address -> nonce)
+
+    /// @notice Current nonce per asset per signer.
+    /// Updating it will invalidate all signatures created with the previous value on an asset level.
+    /// @custom:schema (contract address -> token id -> signer address -> nonce)
     mapping(address => mapping(uint256 => mapping(address => uint256))) public assetNonce;
 
     event ContractNonceUpdated(uint256 _from, uint256 _to, address _sender);
     event SignerNonceUpdated(uint256 _from, uint256 _to, address _signer, address _sender);
     event AssetNonceUpdated(uint256 _from, uint256 _to, address _contractAddress, uint256 _tokenId, address _signer, address _sender);
 
-    // Public
-
-    /**
-    @notice Increase by 1 the contract nonce
-    @dev This can be used to invalidate all signatures created with the previous nonce.
-     */
+    /// @notice As the owner of the contract, increase the contract nonce by 1.
     function bumpContractNonce() external onlyOwner {
         _bumpContractNonce();
     }
 
-    /**
-    @notice Increase by 1 the signer nonce
-    @dev This can be used to invalidate all signatures created by the caller with the previous nonce.
-     */
+    /// @notice Increase the signer nonce of the sender by 1.
     function bumpSignerNonce() external {
         _bumpSignerNonce(_msgSender());
     }
 
-    /**
-    @notice Increase by 1 the asset nonce
-    @dev This can be used to invalidate all signatures created by the caller for a given asset with the previous nonce.
-    @param _contractAddress The contract address of the asset.
-    @param _tokenId The token id of the asset.
-     */
+    /// @notice Increase the asset nonce of the sender by 1.
+    /// @param _contractAddress The contract address of the asset.
+    /// @param _tokenId The token id of the asset.
     function bumpAssetNonce(address _contractAddress, uint256 _tokenId) external {
         _bumpAssetNonce(_contractAddress, _tokenId, _msgSender());
     }
 
-    // Private
-
+    /// @dev Increase the contract nonce by 1
     function _bumpContractNonce() internal {
         uint256 previous = contractNonce;
         contractNonce++;
@@ -53,6 +48,7 @@ abstract contract NonceVerifiable is OwnableUpgradeable {
         emit ContractNonceUpdated(previous, contractNonce, _msgSender());
     }
 
+    /// @dev Increase the signer nonce by 1
     function _bumpSignerNonce(address _signer) internal {
         uint256 previous = signerNonce[_signer];
         signerNonce[_signer]++;
@@ -60,6 +56,7 @@ abstract contract NonceVerifiable is OwnableUpgradeable {
         emit SignerNonceUpdated(previous, signerNonce[_signer], _signer, _msgSender());
     }
 
+    /// @dev Increase the asset nonce by 1
     function _bumpAssetNonce(
         address _contractAddress,
         uint256 _tokenId,
@@ -71,14 +68,17 @@ abstract contract NonceVerifiable is OwnableUpgradeable {
         emit AssetNonceUpdated(previous, assetNonce[_contractAddress][_tokenId][_signer], _contractAddress, _tokenId, _signer, _msgSender());
     }
 
+    /// @dev Reverts if the provided nonce does not match the contract nonce.
     function _verifyContractNonce(uint256 _nonce) internal view {
         require(_nonce == contractNonce, "NonceVerifiable#_verifyContractNonce: CONTRACT_NONCE_MISSMATCH");
     }
 
+    /// @dev Reverts if the provided nonce does not match the signer nonce.
     function _verifySignerNonce(address _signer, uint256 _nonce) internal view {
         require(_nonce == signerNonce[_signer], "NonceVerifiable#_verifySignerNonce: SIGNER_NONCE_MISSMATCH");
     }
 
+    /// @dev Reverts if the provided nonce does not match the asset nonce.
     function _verifyAssetNonce(
         address _contractAddress,
         uint256 _tokenId,
