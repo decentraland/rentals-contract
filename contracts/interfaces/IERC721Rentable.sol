8a3e69533a3b943934732f29dab79520de555ec8
Nando <24811313+fzavalia@users.noreply.github.com>
2022-06-29 17:25:39 -0300

    chore: Add comments to new interface

--- contracts/interfaces/IERC721Rentable.sol
+++ contracts/interfaces/IERC721Rentable.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
+/// @notice Extended ERC721 interface with methods required by the Rentals contract.
 interface IERC721Rentable is IERC721 {
+    /// @dev Updates the operator of the asset.
     function setUpdateOperator(uint256, address) external;
+    /// @dev Checks that the provided fingerprint matches the fingerprint of the composable asset.
     function verifyFingerprint(uint256, bytes memory) external view returns (bool);
 }
