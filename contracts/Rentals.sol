5befc85bb99493607da5e26a02e68b3888993604
Nando <24811313+fzavalia@users.noreply.github.com>
2022-07-04 12:25:00 -0300

    chore: Add target comment

--- contracts/Rentals.sol
+++ contracts/Rentals.sol
@@ -57,6 +57,8 @@ contract Rentals is NonceVerifiable, NativeMetaTransaction, IERC721Receiver {
         uint256[] pricePerDay;
         uint256[] maxDays;
         uint256[] minDays;
+        // Makes the listing acceptable only by the address defined as target. 
+        // Using address(0) as target will allow any other address to accept it.
         address target;
         bytes signature;
     }
