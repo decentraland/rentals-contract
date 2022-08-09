f00543d69e37f729b893df83ef86dec826938502
Nando <24811313+fzavalia@users.noreply.github.com>
2022-08-08 23:31:00 -0300

    chore: Lint

--- contracts/interfaces/IERC721Rentable.sol
+++ contracts/interfaces/IERC721Rentable.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 /// @notice Extended ERC721 interface with methods required by the Rentals contract.
 interface IERC721Rentable is IERC721 {
-    /// @dev Updates the operator of the asset. 
+    /// @dev Updates the operator of the asset.
     /// The idea of this role is mostly of a content operator, a role capable of modifying the content of the asset.
     /// It is not the same as the one defined in the ERC721 standard, which can manipulate the asset in itself.
     function setUpdateOperator(uint256, address) external;
