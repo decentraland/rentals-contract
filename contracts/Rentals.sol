c01e8a7554d77743a980dad56cf1921b7e7b2def
Nando <24811313+fzavalia@users.noreply.github.com>
2022-10-17 16:01:28 -0300

    chore: Remove OwnableUpgradable import

--- contracts/Rentals.sol
+++ contracts/Rentals.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 
