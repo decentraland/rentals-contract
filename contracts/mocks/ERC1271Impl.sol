1da3b6df1139713b870a9266cca2ded91d7e93a9
Nando <24811313+fzavalia@users.noreply.github.com>
2022-11-28 11:33:22 -0300

    feat: Update mock erc1271 impl to recover signer

--- contracts/mocks/ERC1271Impl.sol
+++ contracts/mocks/ERC1271Impl.sol
@@ -2,19 +2,20 @@
 
 pragma solidity 0.8.7;
 
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
+
 /// @dev Mock contract for testing ERC1271 signature verification in the Rentals contract.
 contract ERC1271Impl {
-    bool private immutable isValidSignatureResponse;
+    address private immutable owner;
 
-    constructor(bool _isValidSignatureResponse) {
-        isValidSignatureResponse = _isValidSignatureResponse;
+    constructor(address _owner) {
+        owner = _owner;
     }
 
-    function isValidSignature(
-        bytes32, //_hash
-        bytes memory // _signature
-    ) external view returns (bytes4 magicValue) {
-        if (isValidSignatureResponse) {
+    function isValidSignature(bytes32 _hash, bytes memory _signature) external view returns (bytes4 magicValue) {
+        (address signer, ) = ECDSAUpgradeable.tryRecover(_hash, _signature);
+
+        if (owner == signer) {
             return 0x1626ba7e;
         } else {
             return 0;
